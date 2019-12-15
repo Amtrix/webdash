eb669902a87eeb5298d2bedbcbfa7b542c405623
Damir Ferizovic <amtrix92@gmail.com>
2019-12-15 15:06:22 -0800

    Update

---
+++ data/setup-webdash.sh
@@ -0,0 +1,30 @@
+  
+#/bin/bash
+
+func_localize() {
+    local cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+    local rootdir="$cwd/.."
+
+    local git_webdash_client=https://github.com/Amtrix/src-bin-_webdash-client
+    local git_webdash_server=https://github.com/Amtrix/src-bin-_webdash-server
+    local git_webdash_built_reporter=https://github.com/Amtrix/src-bin-report-build-state
+
+    local webdash_client=$rootdir/src/bin/_webdash-client
+    local webdash_server=$rootdir/src/bin/_webdash-server
+    local webdash_built_reporter=$rootdir/src/bin/report-build-state
+
+    mkdir -pv "$rootdir/app-temporary"
+    mkdir -pv "$rootdir/app-persistent/bin"
+    
+
+    mkdir -pv "$webdash_client"
+    cd "$webdash_client"
+    git clone "$git_webdash_client" .
+    
+    mkdir -pv "$webdash_server"
+    cd "$webdash_server"
+    git clone "$git_webdash_server" .
+}
+
+func_localize
