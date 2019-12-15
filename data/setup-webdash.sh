3a209001ba340f47357268ae1f13d7e26dab4e44
Damir Ferizovic <amtrix92@gmail.com>
2019-12-15 15:07:44 -0800

    Forgot executer lib

--- data/setup-webdash.sh
+++ data/setup-webdash.sh
@@ -6,17 +6,23 @@ func_localize() {
 
     local rootdir="$cwd/.."
 
+    local git_webdash_lib=https://github.com/Amtrix/src-lib-webdash-executer
     local git_webdash_client=https://github.com/Amtrix/src-bin-_webdash-client
     local git_webdash_server=https://github.com/Amtrix/src-bin-_webdash-server
     local git_webdash_built_reporter=https://github.com/Amtrix/src-bin-report-build-state
 
+    local webdash_lib=$rootdir/src/lib/webdash-executer
     local webdash_client=$rootdir/src/bin/_webdash-client
     local webdash_server=$rootdir/src/bin/_webdash-server
     local webdash_built_reporter=$rootdir/src/bin/report-build-state
 
     mkdir -pv "$rootdir/app-temporary"
     mkdir -pv "$rootdir/app-persistent/bin"
-    
+    mkdir -pv "$rootdir/app-persistent/lib"
+
+    mkdir -pv "$webdash_lib"
+    cd "$webdash_lib"
+    git clone "$git_webdash_lib" .
 
     mkdir -pv "$webdash_client"
     cd "$webdash_client"
