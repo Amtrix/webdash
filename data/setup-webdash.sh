d9c594b012d5b1f01da2a42497a941390f6a8070
Damir Ferizovic <amtrix92@gmail.com>
2019-12-15 16:57:19 -0800

    Setup add

--- data/setup-webdash.sh
+++ data/setup-webdash.sh
@@ -3,18 +3,21 @@
 
 func_localize() {
     local cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-    local rootdir="$cwd/.."
+    local rootdir="$( realpath $cwd/..)"
 
     local git_webdash_lib=https://github.com/Amtrix/src-lib-webdash-executer
     local git_webdash_client=https://github.com/Amtrix/src-bin-_webdash-client
     local git_webdash_server=https://github.com/Amtrix/src-bin-_webdash-server
     local git_webdash_built_reporter=https://github.com/Amtrix/src-bin-report-build-state
+    local git_json_lib=https://github.com/nlohmann/json
 
     local webdash_lib=$rootdir/src/lib/webdash-executer
     local webdash_client=$rootdir/src/bin/_webdash-client
     local webdash_server=$rootdir/src/bin/_webdash-server
     local webdash_built_reporter=$rootdir/src/bin/report-build-state
+    local json_lib=$rootdir/src/lib/external/json
+
+    export MYWORLD="$rootdir"
 
     mkdir -pv "$rootdir/app-temporary"
     mkdir -pv "$rootdir/app-persistent/bin"
@@ -31,6 +34,26 @@ func_localize() {
     mkdir -pv "$webdash_server"
     cd "$webdash_server"
     git clone "$git_webdash_server" .
+
+    mkdir -pv "$json_lib"
+    cd "$json_lib"
+    git clone "$git_json_lib" .
+
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt update
+    sudo apt install g++-9
+
+    sudo apt-get install cmake
+    sudo apt-get install make
+    sudo ln -s g++-9 /usr/bin/g++ 
+    sudo apt-get install libboost-dev
+    sudo apt-get install libboost-all-dev
+
+    cd "$webdash_lib"
+    mkdir build
+    cd build
+    cmake ../
+    make
 }
 
 func_localize
