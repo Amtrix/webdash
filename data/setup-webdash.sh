61556362a30ee45dbd1688d9a7a6391f71480d6d
Damir Ferizovic <ferizomir@gmail.com>
2022-09-26 21:41:03 -0700

    Update

--- data/setup-webdash.sh
+++ data/setup-webdash.sh
@@ -143,6 +143,7 @@ func_localize() {
 
     printf '\e[1;33m%-6s\e[m\n' "Clone, call :all, and register projects from webdash-profile.json."
     $MYWORLD/./app-persistent/bin/webdash _internal_:create-project-cloner # || { exit 1; }
+    chmod +x $MYWORLD/./app-persistent/data/webdash-client/initialize-projects.sh
     $MYWORLD/./app-persistent/data/webdash-client/initialize-projects.sh # || { exit 1; }
 
     printf "\n\n"
